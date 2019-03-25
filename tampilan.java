6a91007ebf0ed6813c2f336f077d7be6c3a41b6f
toriq99 <37329011+toriq99@users.noreply.github.com>
2019-03-26 04:24:06 +0700

    Add files via upload

---
+++ tampilan.java
@@ -0,0 +1,19 @@
+public class tampilan{
+	public static void main(String[]args){
+		kartuBimbel kartu=new kartuBimbel("1177","TORIK",11,"IPA","SMA DONG","DI RUMAH","081234567891","XTRA COMBO","2044-2045","LONDON");
+		
+		System.out.println("+================+");
+		System.out.println("+  KARTU BIMBEL  +");
+		System.out.println("+================+\n");
+		System.out.println("ID\t: "+kartu.id());
+		System.out.println("Nama\t: "+kartu.nama());
+		System.out.println("Kelas\t: "+kartu.kelas());
+		System.out.println("Jurusan\t: "+kartu.jurusan());
+		System.out.println("Sekolah\t: "+kartu.sekolah());
+		System.out.println("Alamat\t: "+kartu.alamat());
+		System.out.println("No.HP\t: "+kartu.noHP());
+		System.out.println("Paket\t: "+kartu.paketBimbel());
+		System.out.println("Tahun\t: "+kartu.tahunAjar());
+		System.out.println("Unit\t: "+kartu.unit());
+	}
+}
